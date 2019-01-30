1d338bd479211c9c6a009fd3f37bfba51921fd00
Viktor Mychkin <viktor.mychkin@smarp.com>
2019-01-30 18:46:31 +0200

    init commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,16 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render( < App / > , div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render( < App / > , div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render( < App / > , div);
   ReactDOM.unmountComponentAtNode(div);
 });
